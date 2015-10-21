b7f5a3a78eb48d2e5c91394e240edb3704193def
SophieV <Sophie@adoptive.co>
2015-10-21 16:34:21 -0400

    navigation status highlighted

--- src/scripts/MainContainer.jsx
+++ src/scripts/MainContainer.jsx
@@ -3,10 +3,16 @@ import { Router, Route, Link } from 'react-router'
 
 let MainContainer = React.createClass({
 	render: function() {
+		let homeClassNames = (this.props.location.pathname === '/home'?'active-route':'');
+		let workClassNames = (this.props.location.pathname === '/work'?'active-route':'');
+
 		return (
 			<div className="main-container">
 				<div className="navigation-bar">
-					Menu here. FYI, current route is "{this.props.location.pathname}"
+					<p>
+			          <span className={homeClassNames}><Link to="/home">Home</Link></span> 
+			          <span className={workClassNames}><Link to="/work">Work</Link></span>
+			        </p>
 				</div>
 				{this.props.children || "Welcome to the site"}
 			</div>
